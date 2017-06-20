4e9cf39b6595cecfd1bc1e86e91fb18ca0a0c4e4
Dan <dcam90@gmail.com>
2017-06-20 13:44:14 -0400

    6/20/2017

--- app/src/main/java/multitouch/android/vogella/com/rempas/Main5Activity.java
+++ app/src/main/java/multitouch/android/vogella/com/rempas/Main5Activity.java
@@ -2,6 +2,7 @@ package multitouch.android.vogella.com.rempas;
 
 ////////////////FOR UPDATE
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -32,12 +33,15 @@ public class Main5Activity extends Activity {
         switch (item.getItemId()) {
             case R.id.addition:
                 startActivity(new Intent(this, Main2Activity.class));
+                return true;
             case R.id.retrieval:
                 startActivity(new Intent(this, Main3Activity.class));
+                return true;
             case R.id.replacement:
                 return true;
             case R.id.deletion:
                 startActivity(new Intent(this, Main4Activity.class));
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -45,45 +49,70 @@ public class Main5Activity extends Activity {
 
     public void onClickReplacePass(View view) {
         EditText replacetext = (EditText)findViewById(R.id.replaceEdit);
+        EditText oldtext = (EditText)findViewById(R.id.oldEdit);
+        EditText newtext = (EditText)findViewById(R.id.newEdit);
+        EditText newtextcon = (EditText)findViewById(R.id.newConfEdit);
         final String replace_query = replacetext.getText().toString();
+        final String old_pass = oldtext.getText().toString();
+        final String new_pass = newtext.getText().toString();
+        final String new_pass_con = newtextcon.getText().toString();
 
-        if (replace_query.length() == 0) {
+        if (replace_query.length() == 0 || old_pass.length() == 0 || new_pass.length() == 0 || new_pass_con.length() == 0) {
             Toast.makeText(getBaseContext(),
-                    "The name field is blank, please re-enter.", Toast.LENGTH_LONG).show();
-            return;
+                    "Please fill in all of the fields.", Toast.LENGTH_LONG).show();
         }
 
-        if (!checkIfDataExists(replace_query)) { //can't find the password
+        else if (!checkIfDataExists(replace_query)) { //can't find the name
             Toast.makeText(getBaseContext(),
-                    "Queried password could not be found.", Toast.LENGTH_LONG).show();
+                    "Queried name could not be found.", Toast.LENGTH_LONG).show();
+        }
+
+        else if (!checkIfOldPassMatches(replace_query, old_pass)) {
+            Toast.makeText(getBaseContext(),
+                    "The old password does not match the provided name.", Toast.LENGTH_LONG).show();
+        }
+
+        else if (!new_pass.equals(new_pass_con)) {
+            Toast.makeText(getBaseContext(),
+                    "The new password and its confirmation do not match.", Toast.LENGTH_LONG).show();
         }
 
         else {
-
+            ContentValues values = new ContentValues();
+            values.put(PasswordProvider.PASSWORD, new_pass);
+            getContentResolver().update(PasswordProvider.CONTENT_URI, values, "password = " + old_pass, null);
+            Toast.makeText(getBaseContext(),
+                    "The password for " + replace_query + " has been updated.", Toast.LENGTH_LONG).show();
         }
     }
 
-    private boolean checkIfOldPassMatches(String password) {
+    private boolean checkIfOldPassMatches(String name, String password) {
         String URL = "content://multitouch.android.vogella.com.rempas.PasswordProvider/passwords";
         Uri passwords = Uri.parse(URL);
-        int check = 0;
+        String the_name = " ", the_pass = " ";
 
-        Cursor c = getContentResolver().query(passwords, null, null, null, "password");
+        Cursor c = getContentResolver().query(passwords, null, null, null, "name");
         if (!c.moveToFirst()) {
             return false;
         }
         else {
             c.moveToFirst();
             do {
-                String inTable = c.getString(c.getColumnIndex(PasswordProvider.PASSWORD));
-                if (inTable.equals(password)) {
-                    check = 1;
+                String inTable = c.getString(c.getColumnIndex(PasswordProvider.NAME));
+                if (inTable.equals(name)) {
+                    the_name = inTable;
+                    the_pass = c.getString(c.getColumnIndex(PasswordProvider.PASSWORD));
                     break;
                 }
             } while (c.moveToNext());
             c.close();
-            return check != 0;
         }
+
+        if (name.equals(the_name) && password.equals(the_pass)) {
+            return true;
+        }
+
+        return false;
     }
 
     private boolean checkIfDataExists(String name) {
